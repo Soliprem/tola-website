bc15ee4de02542653fdf6fd64bc8ce296cd1f0a9
Soliprem <franci.solidoro@gmail.com>
2025-12-01 14:30:23 +0100

    initial commit

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "devFlake for tola-website";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    tola = {
+      url = "github:kawayww/tola";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    { nixpkgs, tola, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShells.${system} = {
+        default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            nushell
+            tola.packages.${pkgs.system}.default
+          ];
+          shellHook = ''
+            export SHELL=${pkgs.nushell}/bin/nu
+            exec nu
+          '';
+        };
+      };
+    };
+}
